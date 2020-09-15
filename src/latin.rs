2975b0d6f7a5d284a83b7ef2742abefdbbd1166c
thinkofher <piaskupiasku@gmail.com>
2020-09-15 18:38:27 +0200

    Code refactoring.

--- src/latin.rs
+++ src/latin.rs
@@ -3,7 +3,7 @@ const ILLEGAL_CHARS: &str = ":;.,?!";
 const VOWELS_ADDITION: &str = "hay";
 const CONSONANT_ADDITION: &str = "ay";
 
-enum Letter {
+pub enum Letter {
     Consonant(char),
     Vowel(char),
 }
@@ -22,50 +22,48 @@ fn is_vowel(c: char) -> bool {
     VOWELS.contains(c.to_lowercase().next().unwrap())
 }
 
-fn get_rid_of_chars(mut s: String, chars_slice: &str) -> String {
-    for ch in chars_slice.chars() {
-        s.retain(|c| c != ch)
-    }
-    s
+fn filter_chars(s: String, undesirable: &str) -> String {
+    s.chars().filter(|&c| !undesirable.contains(c)).collect()
 }
 
-fn get_rid_of_illegal_chars(s: String) -> String {
-    get_rid_of_chars(s, ILLEGAL_CHARS)
+fn filter_illegal_chars(s: String) -> String {
+    filter_chars(s, ILLEGAL_CHARS)
 }
 
 pub fn transcript(text: String) -> String {
-    let text_vev: Vec<String> = text
-        .split_whitespace()
+    text.split_whitespace()
         .map(|s| s.to_string())
-        .map(|s| get_rid_of_illegal_chars(s))
-        .map(|s| Word::from(&s[..]).to_pig_latin())
-        .collect();
-    text_vev.join(" ")
+        .map(|s| filter_illegal_chars(s))
+        .map(|s| s.pig_latin())
+        .collect::<Vec<String>>()
+        .join(" ")
 }
 
-struct Word(String);
+pub trait PigLatin {
+    fn first_letter(&self) -> char;
 
-impl Word {
-    fn from(s: &str) -> Word {
-        Word(String::from(s.to_lowercase()))
-    }
-
-    fn to_pig_latin(&self) -> String {
-        match self.starts_with() {
-            Letter::Consonant(c) => format!("{}{}{}", self.rest(), c, CONSONANT_ADDITION),
-            Letter::Vowel(_) => format!("{}{}", self.0, VOWELS_ADDITION),
-        }
-    }
-
-    fn first_letter(&self) -> char {
-        self.0.chars().next().unwrap()
-    }
-
-    fn rest(&self) -> String {
-        self.0.chars().skip(1).collect()
-    }
+    fn rest(&self) -> String;
 
     fn starts_with(&self) -> Letter {
         Letter::new(self.first_letter())
+    }
+
+    fn pig_latin(&self) -> String {
+        match self.starts_with() {
+            Letter::Consonant(c) => format!("{}-{}{}", self.rest(), c, CONSONANT_ADDITION),
+            Letter::Vowel(_) => {
+                format!("{}{}-{}", self.first_letter(), self.rest(), VOWELS_ADDITION)
+            }
+        }
+    }
+}
+
+impl PigLatin for String {
+    fn first_letter(&self) -> char {
+        self.to_lowercase().chars().next().unwrap()
+    }
+
+    fn rest(&self) -> String {
+        self.to_lowercase().chars().skip(1).collect()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ use std::io::prelude::*;
 fn main() {
     let input = io::stdin();
     for line in input.lock().lines() {
-            println!("{}", latin::transcript(line.unwrap()));
+        println!("{}", latin::transcript(line.unwrap()));
     }
 }
